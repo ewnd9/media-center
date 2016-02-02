2432f23ca3c6aa9b2228bbbacd665fddf8727cc6
ewnd9 <ewndnine@gmail.com>
2016-02-02 13:11:53 +0800

    update trakt-utils

--- frontend/components/trakt-report.js
+++ frontend/components/trakt-report.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import _ from 'lodash';
 import Spinner from 'react-spinkit';
 
 import * as api from './../api';
@@ -12,32 +11,7 @@ export default React.createClass({
   getReport: function() {
     api
       .getReport()
-      .then((report) => {
-        const reportArray = _.map(report, (report, show) => ({ show, report }));
-
-        const reportSorted = reportArray.sort((obj1, obj2) => {
-          const getFirstFuture = obj => obj.report.future.length > 0 && obj.report.future[0].episodes[0].first_aired || undefined;
-
-          const first1 = getFirstFuture(obj1);
-          const first2 = getFirstFuture(obj2);
-
-          if (typeof first1 === 'undefined' && typeof first2 === 'undefined') {
-            return 0;
-          }
-
-          if (typeof first1 === 'undefined' && typeof first2 !== 'undefined') {
-            return 1;
-          }
-
-          if (typeof first1 !== 'undefined' && typeof first2 === 'undefined') {
-            return -1;
-          }
-
-          return new Date(first1) - new Date(first2);
-        });
-
-        this.setState({ report: reportSorted, loaded: true });
-      });
+      .then(report => this.setState({ report, loaded: true }));
   },
   componentDidMount: function() {
     this.getReport();
