bb3f1d1e9ba783b564bddc9e37d1a9b50b014207
ewnd9 <ewndnine@gmail.com>
2016-05-18 02:13:06 +0800

    pagination for screenshots

--- src/routes/middleware/pagination.js
+++ src/routes/middleware/pagination.js
@@ -1,8 +1,14 @@
 export default (req, res, next) => {
+  const limit = req.query.limit || 20;
+  const since = req.query.since;
+  const page = req.query.page || 1;
+  const startIndex = (page - 1) * limit;
+
   req.pagination = {
-    limit: req.query.limit || 20,
-    since: req.query.since,
-    page: req.query.page || 1
+    limit,
+    since,
+    page,
+    startIndex
   };
 
   next();
--- src/routes/screenshots.js
+++ src/routes/screenshots.js
@@ -1,15 +1,25 @@
 import express from 'express';
-import fs from 'fs';
 
-export default SCREENSHOTS_PATH => {
+import fs from 'fs';
+import pify from 'pify';
+
+const readdir = pify(fs.readdir);
+
+export default screenshotsPath => {
   const router = express.Router();
 
-  router.get('/api/v1/screenshots', (req, res) => {
-    const files = fs.readdirSync(SCREENSHOTS_PATH);
+  router.get('/api/v1/screenshots', (req, res, next) => {
+    const { startIndex, limit } = req.pagination;
 
-    res.json({
-      files: files.sort().reverse()
-    });
+    readdir(screenshotsPath)
+      .then(_files => {
+        const files = _files.sort().reverse();
+
+        res.json({
+          files: files.slice(startIndex, startIndex + limit)
+        });
+      })
+      .catch(err => next(err));
   });
 
   return router;
