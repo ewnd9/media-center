10717317e86ad58fc162f1e840283d8d34567aa1
ewnd9 <ewndnine@gmail.com>
2016-06-25 12:35:06 +0800

    add npm install caching hack to Dockerfile

--- provision/release-docker.sh
+++ provision/release-docker.sh
@@ -9,6 +9,7 @@ fi
 
 # run from the root of the project
 cp ./provision/docker/rpi-media-center/Dockerfile ./Dockerfile
+node -e "require('fs').writeFileSync('./.deps.json', JSON.stringify({ dependencies: require('./package.json').dependencies }))"
 
 docker build -t ewnd9/media-center .
 docker tag ewnd9/media-center ewnd9/media-center:$1
