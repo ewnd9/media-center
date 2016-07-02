be65277efa8aea92f01565d2858b9fd04af70a69
ewnd9 <ewndnine@gmail.com>
2016-07-02 21:06:28 +0800

    fix trakt test with timefreezing

--- test/routes/trakt-routes-spec.js
+++ test/routes/trakt-routes-spec.js
@@ -1,11 +1,12 @@
 import test from 'ava';
 import 'babel-core/register';
 
+import tk from 'timekeeper';
+
 import createApp from '../fixtures/create-app';
 import agent from '../fixtures/agent';
 import createTrakt from '../fixtures/create-trakt';
 import { showTitle } from '../fixtures/create-fs';
-
 import { nockBefore } from '../helpers/nock';
 
 test.beforeEach(async t => {
@@ -27,8 +28,12 @@ import {
 } from '../fixtures/api-schemas';
 
 test.serial('GET /api/v1/report', async t => {
+  const time = new Date(1467378356960); // july 1st 2016
+  tk.freeze(time);
+
   const { body } = await t.context.request.get('/api/v1/report', {}, traktReportResponseSchema);
   // rejection on schema mismatch
+  tk.reset();
 });
 
 test.serial('GET /api/v1/suggestions', async t => {
