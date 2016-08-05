020fe6da4bd90ce8110b84e3ccb39d096362bd36
ewnd9 <ewndnine@gmail.com>
2016-08-06 03:37:42 +0800

    add dictionary as a source for word model

--- src/models/word.js
+++ src/models/word.js
@@ -1,5 +1,8 @@
 import t from 'tcomb-validation';
 
+const TYPE_BOOK = 'book';
+const TYPE_DICTIONARY = 'dictionary';
+
 export const mediaExampleSourceSchema = t.struct({
   imdb: t.String,
   s: t.maybe(t.Number),
@@ -7,14 +10,29 @@ export const mediaExampleSourceSchema = t.struct({
 });
 
 export const bookExampleSourceSchema = t.struct({
-  type: t.enums.of(['book']),
+  type: t.enums.of([TYPE_BOOK]),
   author: t.String,
   title: t.String
 });
 
-export const exampleSourceSchema = t.union([mediaExampleSourceSchema, bookExampleSourceSchema]);
+export const dictionaryExampleSourceSchema = t.struct({
+  type: t.enums.of([TYPE_DICTIONARY])
+});
+
+export const exampleSourceSchema = t.union([
+  mediaExampleSourceSchema,
+  bookExampleSourceSchema,
+  dictionaryExampleSourceSchema
+]);
+
 exampleSourceSchema.dispatch = function(x) {
-  return x.imdb ? mediaExampleSourceSchema : bookExampleSourceSchema;
+  if (x.imdb) {
+    return mediaExampleSourceSchema;
+  } else if (x.type === TYPE_BOOK) {
+    return bookExampleSourceSchema;
+  } else if (x.type === TYPE_DICTIONARY) {
+    return dictionaryExampleSourceSchema;
+  }
 };
 
 export const wordExampleSchema = t.struct({
@@ -26,10 +44,17 @@ export const wordExampleSchema = t.struct({
 export const wordSchema = t.struct({
   _id: t.maybe(t.String),
   _rev: t.maybe(t.String),
+  _key: t.maybe(t.String),
+
   type: t.String,
   word: t.String,
   isHidden: t.maybe(t.Boolean),
   examples: t.list(wordExampleSchema),
+
+  lastFactor: t.maybe(t.Number),
+  lastSchedule: t.maybe(t.Number),
+  nextRepeat: t.maybe(t.Number),
+
   updatedAt: t.String
 });
 
