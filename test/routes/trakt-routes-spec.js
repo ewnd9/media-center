1cc1400426a8987badb0890b0d3f8621cc38ac37
ewnd9 <ewndnine@gmail.com>
2016-10-31 20:57:05 +0800

    tests: movies models and api

--- test/routes/trakt-routes-spec.js
+++ test/routes/trakt-routes-spec.js
@@ -24,8 +24,11 @@ test.afterEach(t => {
 import {
   statusStringResponse,
   traktShowResponseSchema,
+  traktMovieResponseSchema,
+  traktMoviesResponseSchema,
   traktReportResponseSchema,
-  traktSuggestionsResponseSchema
+  traktSuggestionsResponseSchema,
+  dvdReleasesSuggestionsResponseSchema
 } from '../fixtures/api-schemas';
 
 test.serial('GET /api/v1/trakt/report', async t => {
@@ -49,8 +52,29 @@ test.serial('GET /api/v1/trakt/report', async t => {
 });
 
 test.serial('GET /api/v1/trakt/sync/shows', async t => {
+  const { db: { EpisodeScrobble } } = t.context.app;
+
+  const docs0 = await EpisodeScrobble.findAll();
+  t.truthy(docs0.length === 0);
+
   await t.context.request.get('/api/v1/trakt/sync/shows', {}, statusStringResponse);
   // rejection on schema mismatch
+
+  const docs1 = await EpisodeScrobble.findAll();
+  t.truthy(docs1.length > 1500);
+});
+
+test.serial('GET /api/v1/trakt/sync/movies', async t => {
+  const { db: { MovieScrobble } } = t.context.app;
+
+  const docs0 = await MovieScrobble.findAll();
+  t.truthy(docs0.length === 0);
+
+  await t.context.request.get('/api/v1/trakt/sync/movies', {}, statusStringResponse);
+  // rejection on schema mismatch
+
+  const docs1 = await MovieScrobble.findAll();
+  t.truthy(docs1.length > 200);
 });
 
 test.serial('GET /api/v1/trakt/shows/:imdb', async t => {
@@ -68,8 +92,77 @@ test.serial('GET /api/v1/trakt/shows/:imdb', async t => {
   tk.reset();
 });
 
+test.serial('GET /api/v1/trakt/movies/:imdb', async t => {
+  const { body: { movie } }  = await t.context.request.get('/api/v1/trakt/movies/tt1392190', {}, traktMovieResponseSchema);
+  t.truthy(movie.title === 'Mad Max: Fury Road');
+});
+
 test.serial('GET /api/v1/suggestions', async t => {
   // const { body } = await t.context.request.get('/api/v1/suggestions', { type: 'show', title: showTitle }, traktSuggestionsResponseSchema);
   await t.context.request.get('/api/v1/suggestions', { type: 'show', title: showTitle }, traktSuggestionsResponseSchema);
   // rejection on schema mismatch
+});
+
+test.serial('GET /api/v1/dvdreleasesdates/suggestions', async t => {
+  const { body } = await t.context.request.get('/api/v1/dvdreleasesdates/suggestions', { query: 'captain america' }, dvdReleasesSuggestionsResponseSchema);
+  // rejection on schema mismatch
+
+  t.truthy(body.suggestions[0].title === 'Captain America: The Winter Soldier');
+  t.truthy(body.suggestions[1].title === 'Captain America 3 Civil War');
+});
+
+test.serial('POST /api/v1/trakt/movies/release-date', async t => {
+  const imdb = 'tt1392190';
+
+  const fn0 = t.context.request.get(`/api/v1/trakt/movies/${imdb}`, {}, traktMovieResponseSchema);
+  const { body: { movie: { releaseDate: releaseDate0 } } } = await fn0;
+
+  t.truthy(!releaseDate0);
+
+  const query = {
+    imdb,
+    releaseDate: new Date().toISOString()
+  };
+
+  const fn1 = t.context.request.postQuery('/api/v1/trakt/movies/release-date', query, traktMovieResponseSchema);
+  const { body: { movie: { releaseDate: releaseDate1 } } } = await fn1;
+  // rejection on schema mismatch
+
+  t.truthy(releaseDate1);
+});
+
+test.serial('GET /api/v1/trakt/movies', async t => {
+  const imdb = 'tt1392190';
+
+  const query = {
+    imdb,
+    releaseDate: new Date().toISOString()
+  };
+
+  await t.context.request.postQuery('/api/v1/trakt/movies/release-date', query, traktMovieResponseSchema);
+
+  const { body: { movies } } = await t.context.request.get('/api/v1/trakt/movies', {}, traktMoviesResponseSchema);
+  // rejection on schema mismatch
+
+  t.truthy(movies.length === 1);
+  t.truthy(movies[0].imdb === imdb);
+});
+
+test.serial('Movie.releaseDateIndex complex', async t => {
+  const fn = () => t.context.request.get('/api/v1/trakt/movies', {}, traktMoviesResponseSchema).then(({ body }) => body.movies.length);
+  const imdb = 'tt1392190';
+
+  await t.context.request.get(`/api/v1/trakt/movies/${imdb}`, {}, traktMovieResponseSchema);
+  t.truthy((await fn()) === 0);
+
+  const query = {
+    imdb,
+    releaseDate: new Date().toISOString()
+  };
+
+  await t.context.request.postQuery('/api/v1/trakt/movies/release-date', query, traktMovieResponseSchema);
+  t.truthy((await fn()) === 1);
+
+  await t.context.request.get('/api/v1/trakt/sync/movies', {}, statusStringResponse);
+  t.truthy((await fn()) === 0);
 });
