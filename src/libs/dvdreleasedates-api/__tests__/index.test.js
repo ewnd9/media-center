e2eb25faa487bd3e7b15f1b420d814a355273745
ewnd9 <ewndnine@gmail.com>
2016-10-31 22:50:50 +0800

    fix: dvdreleasesdates-api better parsing

--- src/libs/dvdreleasedates-api/__tests__/index.test.js
+++ src/libs/dvdreleasedates-api/__tests__/index.test.js
@@ -1,12 +1,33 @@
 import test from 'ava';
+
+import { nockBefore } from '../../../../test/helpers/nock';
 import * as api from '../';
 
+test.beforeEach(async t => {
+  const nock = nockBefore(__filename, t, __dirname + '/fixtures');
+  t.context.nockEnd = nock.afterFn;
+});
+
+test.afterEach(t => {
+  t.context.nockEnd();
+});
+
 test('search a movie', async t => {
-  const movies = await api.search('captain america');
-  t.truthy(movies.length === 4);
+  let movies;
+  let movie;
 
-  const movie = movies[1];
+  movies = await api.search('captain america');
+  t.truthy(movies.length === 3);
 
-  t.truthy(movie.title === 'Captain America 3 Civil War');
-  t.truthy(movie.releaseDate === '2016-09-12T16:00:00.000Z');
+  movie = movies[0];
+
+  t.truthy(movie.title === 'Captain America 3 Civil War (2016)');
+  t.truthy(movie.releaseDate);
+
+  movies = await api.search('suicide squad');
+  t.truthy(movies.length === 1);
+
+  movie = movies[0];
+  t.truthy(movie.title === 'Suicide Squad (2016)');
+  t.truthy(movie.releaseDate);
 });
