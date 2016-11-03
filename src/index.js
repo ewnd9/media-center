0b4c06eea41d3d65838e5eb7e19281f433ad97c4
ewnd9 <ewndnine@gmail.com>
2016-11-03 14:07:59 +0800

    refactor: replace nodemon+webpack-dev-server to a single babel-watch setup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import createServices from './services/index';
 import createServer from './server';
 
 import * as config from './config';
+import report from './agent';
+
+process.on('uncaughtException', function (err) {
+  console.error('global exception:', err.stack || err);
+  report(err);
+});
+
+process.on('unhandledRejection', function (reason) {
+  console.error('unhandled promise rejection:', reason.stack || reason);
+  report(reason);
+});
 
 export default function start() {
   return createDb(config.dbPath + '/db')
@@ -12,4 +23,8 @@ export default function start() {
       const services = createServices(db, storage, config);
       return createServer({ db, services, config });
     });
+}
+
+if (!module.parent) {
+  start();
 }
