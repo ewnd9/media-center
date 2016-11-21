bd30798863a053f1454df8f738a75ff6b5fcd6b2
Travis-CI <travis@ewnd9.com>
2016-11-21 10:00:53 +0800

    fix: demo: add git user / email to deploy

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 cd demo/public/media-center
 
 git init
 
+git config --global user.email
+
+if [ $? != 0 ]; then
+  git config --global user.email "travis@ewnd9.com"
+  git config --global user.name "Travis-CI"
+fi
+
 git add .
-git commit \
-  -m "Deploy to GitHub Pages ${TRAVIS_COMMIT}" \
-  --author="Travis-CI <travis@ewnd9.com>"
+git commit -m "Deploy to GitHub Pages ${TRAVIS_COMMIT}"
 
 git push --force "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
